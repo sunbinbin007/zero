c6841be495c86f2d576a4bdf26e9faaba0b34342
孙彬彬 <sunbinbin@imdada.cn>
2016-11-07 11:02:05 +0800

    [ADD] vex

--- src/main.js
+++ src/main.js
@@ -1,20 +1,31 @@
 import Vue from 'vue';
 import MintUI from 'mint-ui';
 import VueRouter from 'vue-router';
+import Vuex from 'vuex';
 import 'mint-ui/lib/style.css';
 import App from './app';
 import routes from './route';
 
 Vue.use(MintUI);
 Vue.use(VueRouter);
+Vue.use(Vuex);
+
+const debug = process.env.NODE_ENV !== 'production';
 
 const router = new VueRouter({
   base: __dirname,
   routes
 });
 
+const store = new Vuex.Store({
+  modules: {},
+  strict : debug,
+  plugins: debug ? [createLogger()] : []
+});
+
 new Vue({ // eslint-disable-line
   el: '#app',
   render: h => h(App),
-  router
+  router,
+  store
 });
